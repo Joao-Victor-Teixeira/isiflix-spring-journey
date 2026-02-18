af469f769bb98d7d33504cfe7de47f7607b747c0
JoaoVictorTeixeira <jxvxteixeira777@outlook.com>
2026-02-18 17:54:59 -0300

    Criação do projeto Conference

---
+++ projeto-conferencia/conference/src/test/java/br/com/joaodev/conference/ConferenceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joaodev.conference;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
