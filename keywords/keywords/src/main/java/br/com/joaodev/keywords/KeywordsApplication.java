da8109dfe8ed911b8fbfd55653ac6a247ab4bcc3
JoaoVictorTeixeira <jxvxteixeira777@outlook.com>
2026-02-19 08:05:34 -0300

    Criação do projeto

---
+++ keywords/keywords/src/main/java/br/com/joaodev/keywords/KeywordsApplication.java
@@ -0,0 +1,13 @@
+package br.com.joaodev.keywords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeywordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeywordsApplication.class, args);
+	}
+
+}
